e74260e9eed1f75dd828d47eff554b9c63b2d47a
JoaoHenrique871 <joaotss8771@gmail.com>
2025-11-13 19:03:11 -0300

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({  base: '/Macpet-Site/',
+export default defineConfig({
   base: '/Macpet-Site/',
   plugins: [react(),tailwindcss()],
   resolve: {
